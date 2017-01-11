b3a34df99ebb30a968edf411bea7519790a44aed
Thomas Müller <mimmi20@live.de>
2017-01-11 20:23:21 +0100

    rewrite class

--- src/WhichBrowserSource.php
+++ src/WhichBrowserSource.php
@@ -22,29 +22,43 @@ class WhichBrowserSource implements SourceInterface
      */
     public function getUserAgents(Logger $logger, OutputInterface $output, $limit = 0)
     {
+        $counter   = 0;
         $allAgents = [];
 
-        foreach ($this->loadFromPath($output) as $dataFile) {
-            if ($limit && count($allAgents) >= $limit) {
-                break;
+        foreach ($this->loadFromPath($output) as $data) {
+            if ($limit && $counter >= $limit) {
+                return;
             }
 
-            $agentsFromFile = $this->mapWhichbrowser($dataFile);
+            foreach ($data as $row) {
+                if ($limit && $counter >= $limit) {
+                    return;
+                }
 
-            $output->writeln(' [added ' . str_pad(number_format(count($allAgents)), 12, ' ', STR_PAD_LEFT) . ' agent' . (count($allAgents) !== 1 ? 's' : '') . ' so far]');
+                if (!isset($row['headers']['User-Agent'])) {
+                    $headers = http_parse_headers($row['headers']);
 
-            $newAgents = array_diff($agentsFromFile, $allAgents);
-            $allAgents = array_merge($allAgents, $newAgents);
-        }
+                    if (! isset($headers['User-Agent'])) {
+                        continue;
+                    }
 
-        $i = 0;
-        foreach ($allAgents as $agent) {
-            if ($limit && $i >= $limit) {
-                return null;
+                    $agent = $headers['User-Agent'];
+                } else {
+                    $agent = $row['headers']['User-Agent'];
+                }
+
+                if (empty($agent)) {
+                    continue;
+                }
+
+                if (array_key_exists($agent, $allAgents)) {
+                    continue;
+                }
+
+                yield $agent;
+                $allAgents[$agent] = 1;
+                ++$counter;
             }
-
-            ++$i;
-            yield $agent;
         }
     }
 
@@ -58,16 +72,30 @@ class WhichBrowserSource implements SourceInterface
     {
         $allTests = [];
 
-        foreach ($this->loadFromPath($output) as $dataFile) {
-            $agentsFromFile = $this->mapWhichbrowser($dataFile);
+        foreach ($this->loadFromPath($output) as $data) {
+            foreach ($data as $row) {
+                if (!isset($row['headers']['User-Agent'])) {
+                    $headers = http_parse_headers($row['headers']);
 
-            foreach ($agentsFromFile as $ua) {
-                if (array_key_exists($ua, $allTests)) {
+                    if (! isset($headers['User-Agent'])) {
+                        continue;
+                    }
+
+                    $agent = $headers['User-Agent'];
+                } else {
+                    $agent = $row['headers']['User-Agent'];
+                }
+
+                if (empty($agent)) {
                     continue;
                 }
 
-                $allTests[$ua] = [
-                    'ua'         => $ua,
+                if (array_key_exists($agent, $allTests)) {
+                    continue;
+                }
+
+                $test = [
+                    'ua'         => $agent,
                     'properties' => [
                         'Browser_Name'            => null,
                         'Browser_Type'            => null,
@@ -93,13 +121,10 @@ class WhichBrowserSource implements SourceInterface
                         'RenderingEngine_Maker'   => null,
                     ],
                 ];
-            }
-        }
 
-        $i = 0;
-        foreach ($allTests as $ua => $test) {
-            ++$i;
-            yield [$ua => $test];
+                yield [$agent => $test];
+                $allTests[$agent] = 1;
+            }
         }
     }
 
@@ -138,32 +163,5 @@ class WhichBrowserSource implements SourceInterface
                     break;
             }
         }
-    }
-
-    /**
-     * @param array $data
-     *
-     * @return array
-     */
-    private function mapWhichbrowser(array $data)
-    {
-        $allData = [];
-
-        foreach ($data as $row) {
-            if (!isset($row['headers']['User-Agent'])) {
-                $headers = http_parse_headers($row['headers']);
-
-                if (! isset($headers['User-Agent'])) {
-                    continue;
-                }
-
-                $allData[] = $headers['User-Agent'];
-                continue;
-            }
-
-            $allData[] = $row['headers']['User-Agent'];
-        }
-
-        return $allData;
     }
 }
